e476cfe4fceacb0192364743fa16977b2037c05b
CaptainNair01 <nairarn04@gmail.com>
2024-01-24 13:05:02 -0600

    complted weather app service

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7bf05f95acmsh8dd27912578e7fbp17851ejsneb84de4c8655',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "565817e8e6cf396601d14a1dcaaaa10b";
